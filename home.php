edf7c6689a7e6de11367b1a84a272c68d4a76bbf
ilariadilucia <ilariadilucia29@gmail.com>
2017-09-05 17:10:36 +0200

    aggiornato home.php sulla query

--- home.php
+++ home.php
@@ -1,14 +1,22 @@
 <html>
 <head>
 <style>
+table {
+    width: 50%;
+}
+
 
  div {
      width: 50px;
      height: 70px;
-     align-content: center;
+     text-align: center;
+     border-collapse: collapse;
+
     }
   td, th{
-      vertical-align: bottom;
+      text-align: center;
+      border-collapse: collapse;
+
     }
 
 </style>
@@ -31,20 +39,36 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully"."<br>";
 
-$sql = "SELECT user_id, fname, email FROM users";
-$result = $conn->query($sql);
+
+//- parsare giorno
+//- dividere mattina/pomeriggio
+
+$q = "SELECT rs.title, ri.start_date, ri.end_date, r.name
+     FROM resources r
+     INNER JOIN reservation_resources rr
+     ON r.resource_id = rr.resource_id
+     INNER JOIN reservation_series rs
+     ON rs.series_id = rr.series_id
+     INNER JOIN reservation_instances ri
+     ON ri.series_id = rs.series_id";
+
+
+$result = $conn->query($q);
+
 $table="<table cellspacing='2' cellpadding='2'width='450'style='border:1px solid'  >
 
 <tr>
-
 <th>
-<font color='black'>id</font>
+<font color='black'>aula</font>
 </th>
 <th>
-<font color='black'>nome</font>
+<font color='black'>title</font>
 </th>
 <th>
-<font color='black'>email</font>
+<font color='black'>start date</font>
+</th>
+<th>
+<font color='black'>end date</font>
 </th>
 </tr>
 </table>";
@@ -52,30 +76,37 @@ $table="<table cellspacing='2' cellpadding='2'width='450'style='border:1px solid
 echo $table;
 
 if ($result->num_rows > 0) {
-
     // output data of each row
     while($row = $result->fetch_assoc()) {
-    //    echo "user_id: ".$row["user_id"]."<br>";
-         $id=$row['user_id'];
-         $name = $row['fname'];
-         $email=$row['email'];
 
-         $table="<table cellspacing='2' cellpadding='2'width='450'style='border:1px solid'  >
 
+        $title=$row['title'];
+        $start_date = $row['start_date'];
+        $end_date=$row['end_date'];
+         $name = $row['name'];
+
+         $table="<table  cellpadding='2'width='450'style='border:1px solid'  >
          <th>
           <td>
             <div>
-                <font color=black> $id
+                <font color=black> $name
+                </font>
+            </div>
+            </td>
+         </th>
+         <th>
+          <td>
+            <div>
+                <font color=black> $title
                 </font>
             </div>
             </td>
          </th>
 
-
          <th>
           <td>
             <div>
-              <font color=black> $name
+              <font color=black> $start_date
               </font>
             </div>
          </td>
@@ -83,7 +114,7 @@ if ($result->num_rows > 0) {
          <th>
           <td>
             <div>
-            <font color=black>$email</font>
+            <font color=black>$end_date</font>
             </div>
          </th>
          </td>
@@ -93,6 +124,7 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 results";
 }
+
 $conn->close();
 
 ?>
