a41d1ab457dbcc8d31c4af0f6cc94a166c0062cb
alessandraG <alessa.griesi@gmail.com>
2017-09-21 15:39:53 +0200

    Update home.php

--- home.php
+++ home.php
@@ -1,60 +1,66 @@
 <html>
 <head>
 <style>
-table {
-    width: 50%;
+/* Style the header */
+.header {
+    background-color: #ab1f1c;
+    border: 10px;
+    height: 250px;
+    width: 100%;
+    /*background-image:url('logo-red-sapienza.jpg');*/
+    vertical-align: bottom;
+}
+.column.side {
+    width: 25%;
+}
+.footer {
+    background-color: #ab1f1c;
+    padding: 10px;
+    text-align: right;
 }
 
+table {
+    width: 100%;
+}
 
- div {
-     width: 50px;
-     height: 70px;
-     text-align: center;
-     border-collapse: collapse;
-
-    }
-  td, th{
-      text-align: center;
-      border-collapse: collapse;
-
-    }
-
+div {
+    width: 100px;
+    height: 90px;
+    text-align: center;
+   }
+td, th{
+    text-align: center;
+    border-collapse: collapse;
+  }
 </style>
+
 </head>
 <body>
 
 <?php
-
 $servername = "localhost";
 $username = "root";
 $password = "root";
 $dbname = "booked_db";
-
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
 
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully"."<br>";
 
 
-//- dividere mattina/pomeriggio
+//variables
 $current_dateStart = new DateTime("yesterday");
-$current_dateStart->setTime(7, 00, 01);
 $current_dateEnd = new DateTime("tomorrow");
+
+//$current_dateStart->setDate(%Y-%m-%d);
+$current_dateStart->setTime(7, 00,01);
 $current_dateEnd->setTime(23, 59, 59);
 
-
-//echo $current_dateStart->format('Y-m-d H:s:i')."\n";
-//echo $current_dateEnd->format('Y-m-d')."\n";
-
-//$data = date_format($current_dateStart, 'Y/m/d');
-//echo $data;
-
-
-
+//- parsare giorno
+//- dividere mattina/pomeriggio
 $q = "SELECT rs.title, ri.start_date, ri.end_date, r.name
      FROM resources r
      INNER JOIN reservation_resources rr
@@ -64,10 +70,14 @@ $q = "SELECT rs.title, ri.start_date, ri.end_date, r.name
      INNER JOIN reservation_instances ri
      ON ri.series_id = rs.series_id";
 
-//     WHERE date_format(ri.start_date, '%Y-%m-%d') > '2017-09-19'
-//     AND date_format(ri.start_date, '%Y-%m-%d') < '2017-09-21' "
-
 $result = $conn->query($q);
+
+$today_date=date("Y-m-d");
+echo '<div class="header" style="text-align:right;">';
+echo 'Aule prenotate';
+echo '</div>';
+
+echo '<br>';
 
 $table="<table cellspacing='2' cellpadding='2'width='450'style='border:1px solid'  >
 <tr>
@@ -85,23 +95,17 @@ $table="<table cellspacing='2' cellpadding='2'width='450'style='border:1px solid
 </th>
 </tr>
 </table>";
-
 echo $table;
 
 $row = $result->fetch_assoc();
 //echo "query ".$row['name'];
-
-
 $numero_record = $result->num_rows;
-
 //data di oggi
 $p = date('Y-m-d');
 $title=$row['title'];
 $start_date = $row['start_date'];
 $end_date=$row['end_date'];
 $name = $row['name'];
-
-
 for ($i= 0; $i < $numero_record; $i++){
     $row = $result->fetch_assoc();
     $title=$row['title'];
@@ -128,7 +132,6 @@ for ($i= 0; $i < $numero_record; $i++){
            </div>
            </td>
         </th>
-
         <th>
          <td>
            <div>
@@ -148,13 +151,11 @@ for ($i= 0; $i < $numero_record; $i++){
         echo $table;
     }
 }
-
-
-
+echo '<div class="footer">';
+echo 'data di oggi:'."\n".$today_date;
+echo '</div>';
 $conn->close();
-
 ?>
 
 </body>
 </html>
-
